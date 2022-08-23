5d6ce49b3a443d2519ca1812d28357af388b3eea
syed00711 <syed.muzammils@gmail.com>
2022-08-23 09:15:10 +0530

    fibonacci

--- src/com/microcare/loops/Fibonacci.java
+++ src/com/microcare/loops/Fibonacci.java
@@ -1,7 +1,16 @@
 package com.microcare.loops;
 
 public class Fibonacci {
+/*
+ * n=10
+int a=0;
+int b=1;
+int c;
 
+0 1 1 2 3 5 8 11 19 30......
+
+for loop
+ */
 	public static void main(String[] args) {
 		int a=0;
 		int b=1;
@@ -11,6 +20,7 @@ public class Fibonacci {
 		
 		for(int n=0;n<10;n++) {
 			c=a+b;
+			
 			
 			System.out.println(c);
 		}
